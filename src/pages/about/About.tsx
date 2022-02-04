9c238c60b6a8b048f0e24323eab027a8348ebe05
Nathan James Lardizabal <nathanjlardizabal@gmail.com>
2022-02-04 16:09:45 -0500

    landing page section component

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './About.css'
-import Button from '../../components/button/Button';
+import LandingPageSection from '../../components/landing-page-section/LandingPageSection';
 
 function About() {
 	return (
@@ -9,30 +9,9 @@ function About() {
 			<section className="about-banner">
 				<h1 className="about-banner-title">about FCC</h1>
 			</section>
-			<section className="about-beliefs">
-				<div className="about-beliefs-picture"></div>
-				<div className="about-beliefs-container">
-					<h2 className="about-beliefs-title">what we believe</h2>
-					<p className="about-beliefs-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<Button location="/about/beliefs" text="doctrinal statement" />
-				</div>
-			</section>
-			<section className="about-beliefs">
-				<div className="about-beliefs-container">
-					<h2 className="about-beliefs-title">mission & values</h2>
-					<p className="about-beliefs-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<Button location="/about/values" text="mission & values" />
-				</div>
-				<div className="about-beliefs-picture"></div>
-			</section>
-			<section className="about-beliefs">
-				<div className="about-beliefs-picture"></div>
-				<div className="about-beliefs-container">
-					<h2 className="about-beliefs-title">our team</h2>
-					<p className="about-beliefs-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<Button location="/about/leadership" text="leadership team" />
-				</div>
-			</section>
+			<LandingPageSection title="what we believe" btnText="doctrinal statement" btnLocation="/about/beliefs" />
+			<LandingPageSection title="mission & values" btnText="mission & values" btnLocation="/about/values" flip={true}/>
+			<LandingPageSection title="our team" btnText="leadership" btnLocation="/about/leadership" />
 		</div>
 	)
 }
