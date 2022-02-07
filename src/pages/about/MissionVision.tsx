b27d43277418887a3da1812f37671cbac03525cd
Nathan James Lardizabal <nathanjlardizabal@gmail.com>
2022-02-07 15:55:19 -0500

    mission & vision page

--- src/pages/about/MissionVision.tsx
+++ src/pages/about/MissionVision.tsx
@@ -4,7 +4,15 @@ export const MissionVision = () => {
 	return (
 		<div className="mission-vision">
 			<section className="mission-vision-banner">
-				<h1 className="header">mission & vision</h1>
+				<h1 className="mission-vision-title">mission & vision</h1>
+			</section>
+			<section className="mission">
+				<h2>our mission:</h2>
+				<h3>To introduce and represent Christ through our nurturing, small group-driven ministries in the community.</h3>
+			</section>
+			<section className="vision">
+				<h2>our vision:</h2>
+				<h3>A Christ-centred, Holy Spirit-led community committed to the building of strong relationships and planting of healthy reproducing churches.</h3>
 			</section>
 		</div>
 	)
--- src/pages/home/HomeMission.tsx
+++ src/pages/home/HomeMission.tsx
@@ -3,7 +3,7 @@ import './Home.css';
 
 function HomeMission() {
 	return (
-		<section className="mission">
+		<section className="home-mission">
 			<h2 className="mission-header">
 				Our mission here at Freedom in Christ Church is to introduce and represent Christ through our nurturing, small group-driven ministries in the community.
 			</h2>
