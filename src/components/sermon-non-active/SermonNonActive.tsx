eb515a8718462d29333b499a5e3513c8b48e154b
n9d0g <nathanjlardizabal@gmail.com>
2022-02-09 18:59:22 -0500

    nextgen picture formats

--- src/components/sermon-non-active/SermonNonActive.tsx
+++ src/components/sermon-non-active/SermonNonActive.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from 'react'
 
 function SermonNonActive() {
-	return (
-		<a href="www.google.ca">
-			<h4 className="home-sermons-non-active-date">
-					<span>Jan</span>
-					<em>30</em>
-			</h4>
-			<h4 className="home-sermons-non-active-title">
-				<em>Speaker</em>
-				Living a Life of Thanksgiving
-			</h4>
-		</a>
-	)
+  return (
+    <a href="/sermons">
+      <h4 className="home-sermons-non-active-date">
+        <span>Jan</span>
+        <em>30</em>
+      </h4>
+      <h4 className="home-sermons-non-active-title">
+        <em>Speaker</em>
+        Living a Life of Thanksgiving
+      </h4>
+    </a>
+  )
 }
 
-export default SermonNonActive;
+export default SermonNonActive
