28dd49c5c24d2cc60c0b9740c3810a5700b7e199
n9d0g <nathanjlardizabal@gmail.com>
2022-02-11 17:34:32 -0500

    styled components test

--- src/pages/give/Give.tsx
+++ src/pages/give/Give.tsx
@@ -1,22 +1,55 @@
 import React from 'react'
 import useDocumentTitle from '../../hooks/useDocumentTitle'
-import './Give.css'
+import styled from 'styled-components'
+import leadership_bg from '../../assets/pictures/leadership_bg.avif'
 
 export const Give = () => {
   useDocumentTitle('Give')
 
   return (
-    <div className="give">
-      <section className="give-banner">
-        <h1 className="give-banner-title">give</h1>
-      </section>
-      <section className="give-intro">
-        <p className="give-intro-text">
-          You can give financially in multiple ways, but as our services are temporarily online, 
-          you can give online by sending an e-transfer to{' '}
-          <span className="give-intro-text-email">fcc.fcac@gmail.com</span>.
-        </p>
-      </section>
-    </div>
+    <GiveSection>
+      <GiveBanner>
+        <GiveBannerTitle>give</GiveBannerTitle>
+      </GiveBanner>
+      <GiveIntro>
+        <GiveIntroText>
+          You can give financially in multiple ways, but as our services are
+          temporarily online, you can give online by sending an e-transfer to{' '}
+          <GiveIntroTextEmail>fcc.fcac@gmail.com</GiveIntroTextEmail>.
+        </GiveIntroText>
+      </GiveIntro>
+    </GiveSection>
   )
 }
+
+const GiveSection = styled.div``
+
+const GiveBanner = styled.section`
+  background-image: url(${leadership_bg});
+  background-size: cover;
+  background-position: 50%;
+  padding: 7rem 0;
+`
+
+const GiveBannerTitle = styled.h1`
+  text-align: center;
+  font-size: 4rem;
+  color: var(--main-white);
+`
+
+const GiveIntro = styled.section`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 3rem 0;
+`
+
+const GiveIntroText = styled.p`
+  font-size: 2rem;
+`
+
+const GiveIntroTextEmail = styled.span`
+  font-style: italic;
+`
