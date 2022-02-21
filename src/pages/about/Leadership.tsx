6c8f8912912bbf16fc5a672e6d67117f45ad459e
n9d0g <nathanjlardizabal@gmail.com>
2022-02-21 16:43:54 -0500

    SGs and ministries data fetched

--- src/pages/about/Leadership.tsx
+++ src/pages/about/Leadership.tsx
@@ -53,7 +53,7 @@ const Banner = styled.header<BannerProps>`
   justify-content: center;
   background-size: cover;
   background-position: 50%;
-  background-image: ${props => `url(${props.banner})`};
+  background-image: url(${props => props.banner});
   margin: 0 auto;
   padding: 7rem 0;
   @media (max-width: 60em) {
