e224e378bcfc238c6579849b01e762cfbf873b2c
n9d0g <nathanjlardizabal@gmail.com>
2022-03-13 18:31:38 -0400

    v1.2.4: praise schedule font fix; praise schedule date fix

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -1,41 +1,50 @@
 import styled from 'styled-components'
 
 export const InfoModal = (props: any) => {
-	const showModal = () => {
-		alert(
-			'topic: ' + props.topic +
-			'\nscripture: ' + props.scripture +	
-			'\nsummary: ' + props.summary
-		)
-	}
+  const showModal = () => {
+    alert(
+      'topic: ' +
+        props.topic +
+        '\nscripture: ' +
+        props.scripture +
+        '\nsummary: ' +
+        props.summary
+    )
+  }
 
-	return (
-		<InfoModalButton onClick={showModal}>
-			<p>click here</p>	
-		</InfoModalButton>
-	)
+  return (
+    <InfoModalButton onClick={showModal}>
+      <p>click here</p>
+    </InfoModalButton>
+  )
 }
 
 const InfoModalButton = styled.button`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
   text-decoration: none;
   color: var(--blue);
   background-color: transparent;
-	border: none;
+  border: none;
   transition: color 0.3s cubic-bezier(0.165, 0.84, 0.44, 1),
     box-shadow 0.3s cubic-bezier(0.165, 0.84, 0.44, 1);
-	
-	> p {
-		font-size: 1rem;
-		text-decoration: underline;
-		margin: 0;
-	}
+
+  > p {
+    color: var(--main-black);
+    font-size: 1rem;
+    text-decoration: underline;
+    margin: 0;
+    padding: 0;
+
+    @media (max-width: 60em) {
+      font-size: 0.75rem;
+    }
+  }
 
   &:hover,
   &:focus {
-		cursor: pointer;
-	}
+    cursor: pointer;
+  }
 `
--- src/components/PraiseSchedule.tsx
+++ src/components/PraiseSchedule.tsx
@@ -3,54 +3,56 @@ import styled from 'styled-components'
 import { InfoModal } from './InfoModal'
 
 export const PraiseSchedule = (props: any) => {
-	var today = new Date() 
+  var today = new Date()
 
   // sort and filter schedule by date initially
   const originalPraiseData = props.data
-		.filter((praiseData: any) =>
-			praiseData.date > today.toISOString().split('T')[0])
-		.sort((a: any, b: any) =>
-    	a.date > b.date ? 1 : -1
-		)
+    .filter(
+      (praiseData: any) => praiseData.date >= today.toISOString().split('T')[0]
+    )
+    .sort((a: any, b: any) => (a.date > b.date ? 1 : -1))
 
-	return (
-		<ScheduleContainer>
-			<ScheduleTitle>schedule</ScheduleTitle>
-			<WeekContainer>
-				<WeekDataHeaderContainer>
-					<WeekDataHeader>date</WeekDataHeader>
-					<WeekDataHeader>leader 😎</WeekDataHeader>
-					<WeekDataHeader>guitar 🎸</WeekDataHeader>
-					<WeekDataHeader>piano 🎹</WeekDataHeader>
-					<WeekDataHeader>bass 🤘</WeekDataHeader>
-					<WeekDataHeader>drums 🪘</WeekDataHeader>
-					<WeekDataHeader>backup 🎤</WeekDataHeader>
-					<WeekDataHeader>AV 🔊</WeekDataHeader>
-					<WeekDataHeader>info 📖</WeekDataHeader>
-				</WeekDataHeaderContainer>
-				{originalPraiseData.map((item: any, index: any) => {
-					var first = false;
-					if (index === 0) first = true;
-					return (
-						<Fragment key={index}>
-							<WeekData first={first}>{item.date}</WeekData>
-							<WeekData first={first}>{item.leader}</WeekData>
-							<WeekData first={first}>{item.guitar}</WeekData>
-							<WeekData first={first}>{item.piano}</WeekData>
-							<WeekData first={first}>{item.bass}</WeekData>
-							<WeekData first={first}>{item.drums}</WeekData>
-							<WeekData first={first}>{item.backup}</WeekData>
-							<WeekData first={first}>{item.av}</WeekData>
-							<WeekData first={first}>
-								<InfoModal topic={item.topic} scripture={item.scripture} summary={item.summary} />
-							</WeekData>
-						</Fragment>
-					)
-				})}
-			</WeekContainer>
-		</ScheduleContainer>
-
-	)
+  return (
+    <ScheduleContainer>
+      <ScheduleTitle>schedule</ScheduleTitle>
+      <WeekContainer>
+        <WeekDataHeaderContainer>
+          <WeekDataHeader>📅</WeekDataHeader>
+          <WeekDataHeader>👑</WeekDataHeader>
+          <WeekDataHeader>🎸</WeekDataHeader>
+          <WeekDataHeader>🎹</WeekDataHeader>
+          <WeekDataHeader>🐟</WeekDataHeader>
+          <WeekDataHeader>🪘</WeekDataHeader>
+          <WeekDataHeader>🎤</WeekDataHeader>
+          <WeekDataHeader>🔊</WeekDataHeader>
+          <WeekDataHeader>📖</WeekDataHeader>
+        </WeekDataHeaderContainer>
+        {originalPraiseData.map((item: any, index: any) => {
+          var first = false
+          if (index === 0) first = true
+          return (
+            <Fragment key={index}>
+              <WeekData first={first}>{item.date}</WeekData>
+              <WeekData first={first}>{item.leader}</WeekData>
+              <WeekData first={first}>{item.guitar}</WeekData>
+              <WeekData first={first}>{item.piano}</WeekData>
+              <WeekData first={first}>{item.bass}</WeekData>
+              <WeekData first={first}>{item.drums}</WeekData>
+              <WeekData first={first}>{item.backup}</WeekData>
+              <WeekData first={first}>{item.av}</WeekData>
+              <WeekData first={first}>
+                <InfoModal
+                  topic={item.topic}
+                  scripture={item.scripture}
+                  summary={item.summary}
+                />
+              </WeekData>
+            </Fragment>
+          )
+        })}
+      </WeekContainer>
+    </ScheduleContainer>
+  )
 }
 
 const ScheduleContainer = styled.section`
@@ -59,45 +61,51 @@ const ScheduleContainer = styled.section`
   justify-content: center;
   max-width: 80rem;
   margin: 0 auto;
-	padding-bottom: 3rem;
+  padding-bottom: 3rem;
 `
 
 const ScheduleTitle = styled.h2`
-	display: flex;
-	justify-content: center;
+  display: flex;
+  justify-content: center;
 `
 
 const WeekContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(9, 1fr);
-	overflow: auto;
+  overflow: auto;
 `
 
 interface WeekDataProps {
-	first: boolean 
+  first: boolean
 }
 
 const WeekData = styled.p<WeekDataProps>`
-	display: flex;
+  display: flex;
   justify-content: center;
-	align-items: center;
+  align-items: center;
   margin: 0;
   border: 1px solid var(--secondary-grey);
-	color: ${props => props.first ? `red` : `black`};
-	font-weight: ${props => props.first ? `bold` : `normal`};
+  color: ${props => (props.first ? `red` : `var(--main-black)`)};
+  font-weight: ${props => (props.first ? `bold` : `normal`)};
+  font-size: 1rem;
+
+  @media (max-width: 60em) {
+    font-size: 0.75rem;
+  }
 `
 
 const WeekDataHeaderContainer = styled.div`
-	display: contents;
-	position: sticky;
-	align-self: flex-start;
-	top: 200px;
+  display: contents;
+  position: sticky;
+  align-self: flex-start;
+  top: 200px;
 `
 
 const WeekDataHeader = styled.p`
-	display: flex;
+  display: flex;
   justify-content: center;
+  align-items: center;
   margin: 0;
   border: 1px solid var(--secondary-grey);
-	font-weight: bold;
+  font-weight: bold;
 `
