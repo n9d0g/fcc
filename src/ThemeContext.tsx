dc1b27103aec779f3a809189ee76a1eae347de4a
n9d0g <nathanjlardizabal@gmail.com>
2022-11-02 15:58:09 -0400

    2.0.0: remove CRA stuff

--- src/ThemeContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-
-const ThemeContext = createContext<any>(false)
-
-export default ThemeContext
--- src/pages/sermons/SermonContext.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-
-const SermonContext = createContext<any>(null)
-
-export default SermonContext
