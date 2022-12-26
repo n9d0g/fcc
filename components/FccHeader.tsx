5c679e4dbbe02e89a315bef7dd311e835ca195ee
n9d0g <nathanjlardizabal@gmail.com>
2022-12-25 21:34:45 -0500

    feat(pages): :sparkles: update favicon + header

--- components/FccHeader.tsx
+++ components/FccHeader.tsx
@@ -120,13 +120,13 @@ export default function Example() {
     <header>
       <Popover className="relative bg-white">
         <div className="mx-auto max-w-7xl px-4 sm:px-6">
-          <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex items-center justify-between border-b-2 border-gray-100 py-3 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
               <a href="/">
                 <span className="sr-only">Your Company</span>
                 <img
-                  className="h-8 w-auto sm:h-10"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=600"
+                  className="h-8 w-auto sm:h-20"
+                  src="/fcc-logo.svg"
                   alt=""
                 />
               </a>
