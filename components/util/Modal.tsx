3c2723f706117600f90508b7cd53b7493d649e0a
n9d0g <nathanjlardizabal@gmail.com>
2022-12-27 01:28:18 -0500

    fix(pages): :bug: bleeding modal fix

--- components/util/Modal.tsx
+++ components/util/Modal.tsx
@@ -18,13 +18,13 @@ const Modal = (props: ModalProps) => {
       </button>
       {showModal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none max-w-screen-sm mx-auto">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-nonep">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
+                  <h3 className="text-3xl font-semibold whitespace-pre-line">
                     {props.title} - {props.date}
                   </h3>
                   <button
