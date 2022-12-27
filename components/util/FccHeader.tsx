a1ba086c8c7bb6d4d541e978f8ba770cffe95023
n9d0g <nathanjlardizabal@gmail.com>
2022-12-27 11:41:59 -0500

    fix(nav): :bug: mobile nav z-index fix

--- components/util/FccHeader.tsx
+++ components/util/FccHeader.tsx
@@ -64,7 +64,7 @@ export default function Example() {
           leaveTo="opacity-0 scale-95">
           <Popover.Panel
             focus
-            className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
+            className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden z-10">
             <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
               <div className="px-5 pt-5 pb-6">
                 <div className="flex items-center justify-between">
