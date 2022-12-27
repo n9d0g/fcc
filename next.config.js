b5c77455fb70997ed044172e653bf6158fdb5de4
n9d0g <nathanjlardizabal@gmail.com>
2022-12-27 02:22:59 -0500

    feat(pages): :sparkles: url redirects for praise schedule

--- next.config.js
+++ next.config.js
@@ -2,6 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/sched',
+        destination: '/ministries/praise/schedule',
+        permanent: true,
+      },
+      {
+        source: '/schedule',
+        destination: '/ministries/praise/schedule',
+        permanent: true,
+      },
+      {
+        source: '/ps',
+        destination: '/ministries/praise/schedule',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
