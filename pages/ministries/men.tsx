10c48bfbd2b146856b7d2b3902908ea73f56a2f2
n9d0g <nathanjlardizabal@gmail.com>
2022-12-26 20:36:54 -0500

    feat(pages): :sparkles: ministries landing page

--- pages/ministries/men.tsx
+++ pages/ministries/men.tsx
@@ -3,7 +3,7 @@ import { Layout } from '../../components/util/Layout'
 const Men = () => {
   return (
     <Layout title="FCC | Men">
-      <section>
+      <section className="flex flex-col justify-center items-center w-full px-6">
         <h1 className="text-5xl">Men</h1>
       </section>
     </Layout>
