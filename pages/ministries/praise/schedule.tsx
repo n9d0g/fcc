ce3a5923a78ddd5eabf7b05646ea5b71bae5e0b8
n9d0g <nathanjlardizabal@gmail.com>
2023-01-07 13:14:30 -0500

    feat(praise): :zap: temporal API for praise schedule

--- pages/ministries/praise/schedule.tsx
+++ pages/ministries/praise/schedule.tsx
@@ -1,8 +1,11 @@
+import { Temporal } from '@js-temporal/polyfill'
 import { Layout } from '../../../components/util/Layout'
 import PraiseModal from '../../../components/util/PraiseModal'
 import Schedule from '../../../data/praise.json'
 
 const MessagesPage = () => {
+  const now = Temporal.Now.plainDateISO().toString()
+
   return (
     <Layout title={'FCC | Praise Schedule'}>
       <section className="flex flex-col container gap-6 justify-around items-center p-6 overflow">
@@ -66,45 +69,47 @@ const MessagesPage = () => {
                         </thead>
                       )}
                       <tbody>
-                        <tr className="bg-gray-100 border-b">
-                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                            {week.date}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            {week.lead}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            {week.guitar}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            {week.keys}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            {week.bass}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            {week.drums}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            {week.backup}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            {week.av}
-                          </td>
-                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                            <PraiseModal
-                              date={week.date}
-                              title="Summary"
-                              messageTitle={week.title}
-                              objective={week.objective}
-                              text={week.summary}
-                              topic={week.topic}
-                              scripture={week.scripture}
-                              summary={week.summary}
-                              speaker={week.speaker}
-                            />
-                          </td>
-                        </tr>
+                        {week.dateTemporal >= now && (
+                          <tr className="bg-gray-100 border-b">
+                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                              {week.date}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                              {week.lead}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                              {week.guitar}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                              {week.keys}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                              {week.bass}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                              {week.drums}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                              {week.backup}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                              {week.av}
+                            </td>
+                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                              <PraiseModal
+                                date={week.date}
+                                title="Summary"
+                                messageTitle={week.title}
+                                objective={week.objective}
+                                text={week.summary}
+                                topic={week.topic}
+                                scripture={week.scripture}
+                                summary={week.summary}
+                                speaker={week.speaker}
+                              />
+                            </td>
+                          </tr>
+                        )}
                       </tbody>
                     </>
                   ))}
