aaa178469eaebad26c734edcdeba43f2ebfc0015
n9d0g <nathanjlardizabal@gmail.com>
2023-03-25 12:41:02 -0400

    style(schedule): :lipstick: reorder praise modal metadata

--- src/lib/ministries/praise/PraiseModal.svelte
+++ src/lib/ministries/praise/PraiseModal.svelte
@@ -5,14 +5,14 @@
 
 	// base classes
 	const cCard = 'flex flex-col card p-4 w-modal-slim shadow-xl space-y-4 min-h-fit'
-	const cHeader = 'text-2xl font-bold mb-16'
+	const cHeader = 'text-2xl font-bold'
 </script>
 
 <section class={cCard}>
 	<header class={cHeader}>{$modalStore[0]?.meta[6]}</header>
+	<p><span class="font-bold">Speaker:</span> {$modalStore[0]?.meta[2]}</p>
+	<p><span class="font-bold">Title:</span> {$modalStore[0]?.meta[1]}</p>
 	<p><span class="font-bold">Series:</span><br /> {$modalStore[0]?.meta[0]}</p>
-	<p><span class="font-bold">Sermon Title:</span><br /> {$modalStore[0]?.meta[1]}</p>
-	<p><span class="font-bold">Speaker:</span><br /> {$modalStore[0]?.meta[2]}</p>
 	<p><span class="font-bold">Scripture:</span><br /> {$modalStore[0]?.meta[3]}</p>
 	<p><span class="font-bold">Objective:</span><br /> {$modalStore[0]?.meta[4]}</p>
 	<p><span class="font-bold">Summary:</span><br /> {$modalStore[0]?.meta[5]}</p>
