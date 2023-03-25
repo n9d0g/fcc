4fce915439a51287e9d5d2df96c1f3450abf0b61
n9d0g <nathanjlardizabal@gmail.com>
2023-03-24 20:14:15 -0400

    feat(sermons): :sparkles: sermon modal metadata fix

---
+++ src/lib/sermons/SermonModal.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { modalStore } from '@skeletonlabs/skeleton'
+
+	// props
+	export let parent: any
+
+	// base classes
+	const cCard = 'card p-4 w-modal shadow-xl space-y-4 min-h-fit'
+	const cHeader = 'text-2xl font-bold'
+</script>
+
+<section class={cCard}>
+	<header class={cHeader}>{$modalStore[0]?.meta.title} - {$modalStore[0]?.meta.date}</header>
+	<p>{$modalStore[0]?.meta.speaker}</p>
+	<p>{$modalStore[0]?.meta.scripture}</p>
+	<footer class="modal-footer {parent.regionFooter}">
+		<button class="btn {parent.buttonNeutral}" on:click={parent.onClose}>
+			{parent.buttonTextCancel}
+		</button>
+	</footer>
+</section>
