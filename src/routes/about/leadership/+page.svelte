ff0133ca0e8f5c8306ff404daa2e01924567cce7
n9d0g <nathanjlardizabal@gmail.com>
2023-03-25 04:17:16 -0400

    feat(pages): :sparkles: leadership cards updated

--- src/routes/about/leadership/+page.svelte
+++ src/routes/about/leadership/+page.svelte
@@ -1,7 +1,10 @@
-<script>
+<script lang="ts">
 	import LeadershipCard from '$lib/about/leadership/LeadershipCard.svelte'
 	import FccLayout from '$lib/FccLayout.svelte'
 	import PageTitle from '$lib/PageTitle.svelte'
+	import type { PageData } from './$types'
+
+	export let data: PageData
 </script>
 
 <svelte:head>
@@ -11,15 +14,8 @@
 <FccLayout title="FCC | Leadership">
 	<PageTitle text="Leadership." />
 	<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full gap-4 my-16">
-		<LeadershipCard />
-		<LeadershipCard />
-		<LeadershipCard />
-		<LeadershipCard />
-		<LeadershipCard />
-		<LeadershipCard />
-		<LeadershipCard />
-		<LeadershipCard />
-		<LeadershipCard />
-		<LeadershipCard />
+		{#each data.leaders as leader}
+			<LeadershipCard img={leader.img} name={leader.name} title={leader.title} />
+		{/each}
 	</div>
 </FccLayout>
