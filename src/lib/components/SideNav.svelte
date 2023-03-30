5b1a4987c60801cac2e9b1d357603b9b0ee6082a
n9d0g <nathanjlardizabal@gmail.com>
2023-03-30 03:07:18 -0400

    fix(nav): :bug: remove focus ring on mobile side nav buttons

--- src/lib/components/SideNav.svelte
+++ src/lib/components/SideNav.svelte
@@ -6,7 +6,11 @@
 <Drawer>
 	<section class="lg:hidden flex flex-col gap-8 p-8 w-screen">
 		{#each navOptions() as option}
-			<a on:click={closeSideNav} href={option.href} class="btn flex justify-start outline-0">
+			<a
+				on:click={closeSideNav}
+				href={option.href}
+				class="btn flex justify-start focus:ring-0 focus:ring-offset-0"
+			>
 				{option.title}
 			</a>
 		{/each}
