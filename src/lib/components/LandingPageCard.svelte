33e4b89c4db63008dc9292d606a6a53e18e62b2d
n9d0g <nathanjlardizabal@gmail.com>
2023-03-31 13:20:12 -0400

    style(site): :lipstick: update colours to fcc colours (looks kinda bad lol)

--- src/lib/components/LandingPageCard.svelte
+++ src/lib/components/LandingPageCard.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	export let title: string
 	export let link: string
+
+	// variables
 </script>
 
-<a
-	href={link}
-	class="card card-hover flex justify-center items-center text-center h-64 rounded-md p-4"
->
+<a href={link} class="card card-hover flex justify-center items-center text-center h-64 rounded-md p-4">
 	<h2 class="text-xl">{title}</h2>
 </a>
