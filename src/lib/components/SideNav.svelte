8e4170014bfdd9cb8d4c6e5031134ae1171ba8a2
n9d0g <nathanjlardizabal@gmail.com>
2023-04-02 15:01:21 -0400

    fix(mobile): :bug: make sidenav drawer unscrollable

--- src/lib/components/SideNav.svelte
+++ src/lib/components/SideNav.svelte
@@ -4,13 +4,9 @@
 </script>
 
 <Drawer>
-	<section class="lg:hidden flex flex-col gap-8 p-8 w-screen">
+	<section class="lg:hidden flex flex-col gap-8 p-8 w-screen overflow-hidden">
 		{#each navOptions() as option}
-			<a
-				on:click={closeSideNav}
-				href={option.href}
-				class="btn flex justify-start focus:ring-0 focus:ring-offset-0"
-			>
+			<a on:click={closeSideNav} href={option.href} class="btn flex justify-start focus:ring-0 focus:ring-offset-0">
 				{option.title}
 			</a>
 		{/each}
