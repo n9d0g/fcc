456d9620207d61b5121a3f7818164564b58849ea
n9d0g <nathanjlardizabal@gmail.com>
2023-04-03 03:05:44 -0400

    feat(pages): :sparkles: home page next event section

---
+++ src/lib/components/home/HomeNextEvent.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { links } from '$lib/constants'
+</script>
+
+<section class="container mx-auto flex justify-center align-center my-32">
+	<a href={links.zoom}>
+		<img
+			class="h-[60vh] w-auto"
+			src="https://scontent.fyto1-1.fna.fbcdn.net/v/t1.15752-9/337869538_3451613955123690_7692732717910605794_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=ae9488&_nc_ohc=KUU7I8bouQgAX9XE_Qd&_nc_ht=scontent.fyto1-1.fna&oh=03_AdTS803amrYbZN--HXBODNxpb3-nQiourqt0r4A2-p9CIA&oe=6451DC1B"
+			alt=""
+		/>
+	</a>
+</section>
