c95841cd6348eb38a994022d35e6ec7e84c4f911
n9d0g <nathanjlardizabal@gmail.com>
2023-04-05 14:40:04 -0400

    test(pages): :white_check_mark: update home tests with QA site link

--- tests/home/test.ts
+++ tests/home/test.ts
@@ -1,19 +1,21 @@
 import { expect, test } from '@playwright/test'
 
+const devHome: string = 'https://dev.fcccanada.ca'
+
 test('home: has title', async ({ page }) => {
-  await page.goto('/')
+  await page.goto(devHome)
   await expect(page).toHaveTitle('Freedom in Christ Church.')
 })
 
 test('home: hero has h1', async ({ page }) => {
-  await page.goto('/')
+  await page.goto(devHome)
   await expect(
     page.getByRole('heading', { name: 'Welcome to Freedom in Christ Church.' })
   ).toBeVisible()
 })
 
 test('home: hero has h3', async ({ page }) => {
-  await page.goto('/')
+  await page.goto(devHome)
   await expect(
     page.getByRole('heading', {
       name: 'A Christ-centred, Holy Spirit-led community committed to the building of strong relationships and planting of healthy reproducing churches.',
