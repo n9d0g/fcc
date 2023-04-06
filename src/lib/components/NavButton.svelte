fca11bb5dd7cfca7ebd8f5af63f3f000afc97c00
n9d0g <nathanjlardizabal@gmail.com>
2023-04-06 13:17:40 -0400

    fix(nav): :bug: revert nav buttons to w-32 - laptop issues

--- src/lib/components/NavButton.svelte
+++ src/lib/components/NavButton.svelte
@@ -10,8 +10,8 @@
 	if (link === '/' && nav === 'home') activeNav = true
 	else if (link.includes(nav)) activeNav = true
 
-	const nonActiveClass = 'flex w-fit btn hover:variant-filled-surface h-10 btn-icon p-4 z-20 rounded-lg'
-	const activeClass = 'flex w-fit btn h-10 btn-icon p-4 z-20 font-bold variant-filled-surface rounded-lg'
+	const nonActiveClass = 'flex w-32 btn hover:variant-filled-surface h-10 btn-icon p-4 z-20 rounded-lg'
+	const activeClass = 'flex w-32 btn h-10 btn-icon p-4 z-20 font-bold variant-filled-surface rounded-lg'
 </script>
 
 <a href={link} class={!activeNav ? nonActiveClass : activeClass}>
