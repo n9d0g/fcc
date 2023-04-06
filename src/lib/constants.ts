17dce4b85b3181584367da3e8df69ece88fa762e
n9d0g <nathanjlardizabal@gmail.com>
2023-04-06 09:19:39 -0400

    docs(pages): :memo: update copy for give page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -108,6 +108,11 @@ export const links = {
   googleMaps:
     'https://www.google.ca/maps/place/5225+Orbitor+Dr,+Mississauga,+ON+L4W+4Y8/@43.6598788,-79.6065117,17z/data=!3m1!4b1!4m6!3m5!1s0x882b38554ef96393:0x5a2b697b840d7a7d!8m2!3d43.6598749!4d-79.604323!16s%2Fg%2F11b8v4jvnm',
   zoom: 'https://us04web.zoom.us/j/3926981154?pwd=d1BHRTB3eStxZFZIS3lyWHBieENvZz09&fbclid=IwAR1N4y1LI5zX5bPnAABrXd_8ICuFko9Hw_5HV2P4wWj7U6JmALskzjO3H_U',
+  giveVerse1:
+    'https://www.biblegateway.com/passage/?search=2%20Corinthians%209%3A7&version=NIV',
+  giveVerse2:
+    'https://www.biblegateway.com/passage/?search=2+Corinthians+9%3A8&version=NIV',
+  etransfer: 'emailto:fcc.fcac@gmail.com',
 }
 
 export const leaders = [
---
+++ src/routes/give/+page.ts
@@ -0,0 +1,14 @@
+import type { PageLoad } from './$types'
+import { links } from '$lib/constants'
+
+export const load = (async () => {
+  return {
+    links: {
+      verse1: links.giveVerse1,
+      verse2: links.giveVerse2,
+      googleMaps: links.googleMaps,
+      etransfer: links.etransfer,
+      pic1: 'https://images.unsplash.com/photo-1499652848871-1527a310b13a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+    },
+  }
+}) satisfies PageLoad
