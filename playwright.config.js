a70766d2b7eb06c17ca981990814a72a114d8c76
n9d0g <nathanjlardizabal@gmail.com>
2023-05-25 14:42:57 -0400

    fix(workflows): :bug: remove dev/prod folders; 1 consistent folder for tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,12 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-	testDir: 'tests',
-	fullyParallel: true,
-	webServer: {
-		command: 'pnpm build && pnpm preview',
-		port: 4173,
-	},
-	reporter: [['html', { open: 'always' }]],
+  testDir: 'tests',
+  fullyParallel: true,
+  webServer: {
+    command: 'pnpm build && pnpm preview',
+    port: 4173,
+  },
+  // reporter: [['html', { open: 'always' }]],
 }
 
 export default config
