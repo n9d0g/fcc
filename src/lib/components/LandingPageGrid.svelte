5a838ff2f818efdd94b4ef5ec6ba5edb0ba03f59
n9d0g <nathanjlardizabal@gmail.com>
2023-08-13 00:38:11 -0400

    refactor(pages): :lipstick: landing page grid -> `logo-cloud` style

--- src/lib/components/LandingPageGrid.svelte
+++ src/lib/components/LandingPageGrid.svelte
@@ -2,9 +2,9 @@
 	export let items: any
 </script>
 
-<div class="my-8 grid w-full grid-cols-1 gap-4 sm:grid-cols-2 md:my-16 lg:grid-cols-3">
+<div class="logo-cloud my-8 grid-cols-1 gap-2 sm:grid-cols-2 md:my-16 lg:grid-cols-3">
 	{#each items as card}
-		<a href={card.link} class="card card-hover flex items-center justify-center rounded-md p-4 text-center md:h-64">
+		<a href={card.link} class="logo-item flex items-center justify-center rounded-sm p-4 text-center md:h-64">
 			<h2 class="h2">{card.title}</h2>
 		</a>
 	{/each}
