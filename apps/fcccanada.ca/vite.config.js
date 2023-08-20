a043628fadeb3651deb0c8d685b7e3cb0fb24bb9
n9d0g <nathanjlardizabal@gmail.com>
2023-08-19 22:55:57 -0400

    fix: :bug: vite import fix

--- apps/fcccanada.ca/vite.config.js
+++ apps/fcccanada.ca/vite.config.js
@@ -3,6 +3,12 @@ import { defineConfig } from 'vitest/config'
 import { execSync } from 'child_process'
 
 export default defineConfig({
+	server: {
+		port: 42069,
+		fs: {
+			allow: ['../../']
+		}
+	},
 	plugins: [sveltekit()],
 	define: {
 		__LAST_COMMIT__: JSON.stringify(execSync('git rev-parse HEAD').toString().trim()),
