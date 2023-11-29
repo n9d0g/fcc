27f2cd88fb4fef508a8e33b4bbae0227ee5ee57c
n9d0g <nathanjlardizabal@gmail.com>
2023-11-29 16:47:40 -0500

    test(web): :test_tube: placeholder awaiting test

--- apps/web/src/routes/ministries/praise/schedule/+page.svelte
+++ apps/web/src/routes/ministries/praise/schedule/+page.svelte
@@ -59,104 +59,110 @@
 </script>
 
 <FccLayout {title} {breadcrumb} {headData}>
-	<div class="relative my-8 flex max-w-fit flex-col gap-4">
-		<!-- filter searching -->
-		<label class="label" for="autocomplete">Filter by leader:</label>
-		<input
-			id="autocomplete"
-			class="autocomplete input w-64 max-w-full"
-			type="text"
-			placeholder="Filter by leader"
-			bind:value={leader}
-			use:popup={praiseFilterPopupSettings}
-			data-testid="schedule-search"
-		/>
-		{#if leader.length > 0}
-			<button
-				transition:fade={{ duration: 150 }}
-				on:click={() => (leader = '')}
-				class="absolute right-2 top-[47px] h-7 w-7 cursor-pointer rounded-xl"
-			>
-				<IoIosClose />
-			</button>
-		{/if}
-	</div>
-
-	<!-- autocomplete modal -->
-	<div
-		data-popup="praiseAutocomplete"
-		class="bg-surface-100-800-token z-30 w-64 rounded-md p-4 text-left"
-	>
-		<Autocomplete
-			bind:input={leader}
-			options={praiseLeaderOptions}
-			on:selection={onFilterSelection}
-			emptyState="No results found 🙈"
-			class=""
-		/>
-	</div>
-
-	<DetailsTooltip />
-
-	<!-- schedule table -->
-	<div bind:this={schedTable}>
-		<div class="table-container relative h-[60vh] w-full">
-			<table
-				class="table table-hover table-compact relative overflow-scroll"
-				data-testid="schedule-table"
-			>
-				<thead>
-					<tr class="variant-filled-secondary sticky top-0 z-10">
-						{#each tHead as header, index}
-							{#if index === 0}
-								<th
-									class="variant-filled-secondary table-cell-fit sticky left-0 z-30 p-3 text-left font-bold"
-								>
-									{header}
-								</th>
-							{:else}
-								<th class="table-cell-fit p-3 text-left font-bold">{header}</th>
-							{/if}
-						{/each}
-					</tr>
-				</thead>
-				{#key leader}
-					<tbody>
-						{#each searchFilter(upToDatePraiseData, 'lead', leader) as week}
-							<tr on:click={() => openDetails(week)} class="cursor-pointer">
-								{#each tBody as col}
-									{#if week[col]}
-										{#if col === 'date'}
-											<td
-												class="bg-surface-100-800-token table-cell-fit sticky left-0 pl-3 text-left"
-											>
-												{getMonthDay(week[col])}
-											</td>
-										{:else}
-											<td class="whitespace-nowrap pl-3 text-left"
-												>{week[col]}</td
-											>
-										{/if}
-									{:else}
-										<td class="text-left" />
-									{/if}
-								{/each}
-							</tr>
-						{/each}
-					</tbody>
-				{/key}
-			</table>
+	{#await data}
+		<div>awaiting data...</div>
+	{:then data}
+		<div class="relative my-8 flex max-w-fit flex-col gap-4">
+			<!-- filter searching -->
+			<label class="label" for="autocomplete">Filter by leader:</label>
+			<input
+				id="autocomplete"
+				class="autocomplete input w-64 max-w-full"
+				type="text"
+				placeholder="Filter by leader"
+				bind:value={leader}
+				use:popup={praiseFilterPopupSettings}
+				data-testid="schedule-search"
+			/>
+			{#if leader.length > 0}
+				<button
+					transition:fade={{ duration: 150 }}
+					on:click={() => (leader = '')}
+					class="absolute right-2 top-[47px] h-7 w-7 cursor-pointer rounded-xl"
+				>
+					<IoIosClose />
+				</button>
+			{/if}
 		</div>
-	</div>
 
-	<!-- link to excel sheet -->
-	<div class="my-8 flex w-full justify-center">
-		<a href={links.excel} target="_blank" class="variant-filled btn">
-			Excel Data Sheet
-		</a>
-	</div>
+		<!-- autocomplete modal -->
+		<div
+			data-popup="praiseAutocomplete"
+			class="bg-surface-100-800-token z-30 w-64 rounded-md p-4 text-left"
+		>
+			<Autocomplete
+				bind:input={leader}
+				options={praiseLeaderOptions}
+				on:selection={onFilterSelection}
+				emptyState="No results found 🙈"
+				class=""
+			/>
+		</div>
 
-	<hr />
+		<DetailsTooltip />
 
-	<WorshipAssignments worshipAssignmentsData={worshipAssignments} />
+		<!-- schedule table -->
+		<div bind:this={schedTable}>
+			<div class="table-container relative h-[60vh] w-full">
+				<table
+					class="table-hover table-compact relative table overflow-scroll"
+					data-testid="schedule-table"
+				>
+					<thead>
+						<tr class="variant-filled-secondary sticky top-0 z-10">
+							{#each tHead as header, index}
+								{#if index === 0}
+									<th
+										class="variant-filled-secondary table-cell-fit sticky left-0 z-30 p-3 text-left font-bold"
+									>
+										{header}
+									</th>
+								{:else}
+									<th class="table-cell-fit p-3 text-left font-bold"
+										>{header}</th
+									>
+								{/if}
+							{/each}
+						</tr>
+					</thead>
+					{#key leader}
+						<tbody>
+							{#each searchFilter(upToDatePraiseData, 'lead', leader) as week}
+								<tr on:click={() => openDetails(week)} class="cursor-pointer">
+									{#each tBody as col}
+										{#if week[col]}
+											{#if col === 'date'}
+												<td
+													class="bg-surface-100-800-token table-cell-fit sticky left-0 pl-3 text-left"
+												>
+													{getMonthDay(week[col])}
+												</td>
+											{:else}
+												<td class="whitespace-nowrap pl-3 text-left"
+													>{week[col]}</td
+												>
+											{/if}
+										{:else}
+											<td class="text-left" />
+										{/if}
+									{/each}
+								</tr>
+							{/each}
+						</tbody>
+					{/key}
+				</table>
+			</div>
+		</div>
+
+		<!-- link to excel sheet -->
+		<div class="my-8 flex w-full justify-center">
+			<a href={links.excel} target="_blank" class="variant-filled btn">
+				Excel Data Sheet
+			</a>
+		</div>
+
+		<hr />
+
+		<WorshipAssignments worshipAssignmentsData={worshipAssignments} />
+	{/await}
 </FccLayout>
