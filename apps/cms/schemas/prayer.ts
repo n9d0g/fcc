e787b321fb6efd5f121604bb0ff6b1bbb83bae64
n9d0g <nathanjlardizabal@gmail.com>
2024-01-01 00:59:48 -0500

    refactor(cms): :recycle: add descriptions + fields to `sunday-school` schema

--- apps/cms/schemas/prayer.ts
+++ apps/cms/schemas/prayer.ts
@@ -1,22 +1,25 @@
 export default {
-  name: 'prayer',
-  type: 'document',
-  title: 'Prayer',
-  fields: [
-    {
-      name: 'name',
-      title: 'Name',
-      type: 'string',
-    },
-    {
-      name: 'date',
-      title: 'Date',
-      type: 'date',
-    },
-    {
-      name: 'scripture',
-      title: 'Scripture',
-      type: 'string',
-    },
-  ],
+	name: 'prayer',
+	type: 'document',
+	title: 'Prayer',
+	fields: [
+		{
+			name: 'name',
+			title: 'Name',
+			description: 'Name of person speaking for given prayer meeting.',
+			type: 'string',
+		},
+		{
+			name: 'date',
+			title: 'Date',
+			description: 'Date for given prayer meeting.',
+			type: 'date',
+		},
+		{
+			name: 'scripture',
+			title: 'Scripture',
+			description: 'Scripture passage for given prayer meeting.',
+			type: 'string',
+		},
+	],
 }
