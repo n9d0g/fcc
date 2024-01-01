d1a84162092279a99bd9da9fe591b38dbdb63573
n9d0g <nathanjlardizabal@gmail.com>
2023-12-31 23:39:12 -0500

    feat(web): :sparkles: simple table on `/sunday-school/toddler` page; to be filled through cms

---
+++ apps/web/src/routes/sunday-school/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import FccLayout from '$lib/components/FccLayout.svelte'
+	import LandingPageGrid from '$lib/components/LandingPageGrid.svelte'
+
+	export let data
+	const { title, breadcrumb, headData } = data
+</script>
+
+<FccLayout {title} {breadcrumb} {headData}>
+	<LandingPageGrid items={data.landingPageItems} />
+</FccLayout>
---
+++ apps/web/src/routes/sunday-school/toddler/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import FccLayout from '$lib/components/FccLayout.svelte'
+
+	export let data
+	const { title, breadcrumb, headData } = data
+</script>
+
+<FccLayout {title} {breadcrumb} {headData}>
+	<div class="table-container">
+		<table class="table-compact table-interactive table">
+			<thead>
+				<tr>
+					<th>date</th>
+					<th>teacher</th>
+					<th>lesson title</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>1</td>
+					<td>2</td>
+					<td>3</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</FccLayout>
