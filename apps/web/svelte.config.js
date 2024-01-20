2c9d5665680a212e68065ae1fb2c3f2de024d107
n9d0g <nathanjlardizabal@gmail.com>
2024-01-20 14:57:10 -0500

    chore(web): :arrow_up: upgrade to svelte 2

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-auto'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import seqPreprocessor from 'svelte-sequential-preprocessor'
 import { readFileSync } from 'fs'
 import { fileURLToPath } from 'url'
