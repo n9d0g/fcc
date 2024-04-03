10a4824e356251f160e42bb22805ea77ef1985ac
Nathan Lardizabal <nate@njil.dev>
2024-04-03 12:33:31 -0400

    fix(web): :safety_vest: add `zod` validation to contact form

--- apps/web/src/routes/contact/+page.server.ts
+++ apps/web/src/routes/contact/+page.server.ts
@@ -1,17 +1,38 @@
 import { GOOGLE_EMAIL } from '$env/static/private'
 import transporter from '$lib/emailSetup.server.js'
+import { z } from 'zod'
+
+const emailSchema = z.object({
+	name: z.string().trim().min(1, { message: 'Name is required' }),
+	email: z.string().trim().email().min(1, { message: 'Email is required' }),
+	message: z.string().trim().min(1, { message: 'Message is required' }),
+})
 
 export const actions = {
 	default: async ({ request }: { request: any }) => {
 		try {
-			const formData = await request.formData()
+			const formData = Object.fromEntries(await request.formData())
+			const emailData = emailSchema.safeParse(formData)
+
+			// validate contact fields
+			if (emailData.success === false) {
+				const errors = emailData.error.flatten().fieldErrors
+				console.log(errors)
+
+				return {
+					status: 400,
+					errors: errors,
+				}
+			}
 
 			const name = formData.get('name')
+			const email = formData.get('email')
 			const message = formData.get('message')
 
 			let html = `
 			<section>
 				<h1>Name: ${name}</h1>
+				<h2>Email: ${email}</h2>
 				<p>Message: <br /> ${message}</p>
 			</section>
 			`
