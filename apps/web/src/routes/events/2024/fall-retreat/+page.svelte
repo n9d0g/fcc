9c8a121f150d5a36dd402fcb97c6a91edabc6578
Nathan Lardizabal <nate@njil.dev>
2024-10-24 22:28:46 -0400

    feat(web): :sparkles: events page -> past events section

---
+++ apps/web/src/routes/events/2024/fall-retreat/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import FccLayout from '$lib/components/FccLayout.svelte'
+
+	export let data
+	const { title, breadcrumb, headData } = data
+</script>
+
+<FccLayout {title} {breadcrumb} {headData}>
+	<div class="container mx-auto">
+		<div>
+			<h2 class="h2 mb-3 font-bold">Worksheets</h2>
+
+			<div class="flex flex-col gap-2">
+				<a
+					href="/pdf/2024/fall-retreat/session-1.pdf"
+					class="anchor w-fit"
+					target="_blank"
+				>
+					Session #1
+				</a>
+				<a
+					href="/pdf/2024/fall-retreat/session-2.pdf"
+					class="anchor w-fit"
+					target="_blank"
+				>
+					Session #2
+				</a>
+				<a
+					href="/pdf/2024/fall-retreat/session-3.pdf"
+					class="anchor w-fit"
+					target="_blank"
+				>
+					Session #3
+				</a>
+				<a
+					href="/pdf/2024/fall-retreat/session-4.pdf"
+					class="anchor w-fit"
+					target="_blank"
+				>
+					Session #4
+				</a>
+			</div>
+		</div>
+	</div>
+</FccLayout>
