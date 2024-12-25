f40f1e72ff1f0c94c82f48be037a40d00f67e881
Nathan Lardizabal <nate@njil.dev>
2024-12-24 19:55:31 -0500

    feat(web): :sparkles: sunday school updates
    #337

--- apps/web/src/routes/ministries/sunday-school/+page.ts
+++ apps/web/src/routes/ministries/sunday-school/+page.ts
@@ -1,6 +1,8 @@
-import { headData, breadcrumbs } from '$lib/constants'
+import { client, headData, breadcrumbs } from '$lib/constants'
+import { updatedDataFiltered } from '$lib/utils'
 
 export const load = async () => {
+	const data = await client.fetch(`*[_type == "sunday-ministries"]`)
 	const breadcrumb = [
 		breadcrumbs.home,
 		breadcrumbs.ministries,
@@ -11,5 +13,12 @@ export const load = async () => {
 		title: 'FCC Sunday School Ministry.',
 		breadcrumb: breadcrumb,
 		headData: headData.sundaySchool,
+		tBody: updatedDataFiltered(data, 'date'),
+		tHeaders: [
+			'Month',
+			'Crawlers & Toddlers',
+			'SS Grades 1-3',
+			'SS Grades 4-6',
+		],
 	}
 }
