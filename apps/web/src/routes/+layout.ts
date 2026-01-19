5c653bba508cada3e1311d51370796bea4fc1cfc
Nathan Lardizabal <nate@njil.dev>
2026-01-19 13:55:57 -0500

    perf(web): :zap: remove unused store subscription in layout

--- apps/web/src/routes/+layout.ts
+++ apps/web/src/routes/+layout.ts
@@ -1,11 +1,7 @@
 import { client } from '$lib/constants'
-import { activePath } from '$lib/stores/store.js'
 
 export const load = async ({ url }) => {
 	const banner = await client.fetch(`*[_type == "banner"]`)
-
-	let activePathValue: string = ''
-	activePath.subscribe((value) => (activePathValue = value))
 
 	return {
 		url: url.pathname,
